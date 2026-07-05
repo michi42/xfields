8e9affd7e93c09c753ea3088c7f28041f287c2e2
Michi <michi.hostettler@cern.ch>
2026-07-05 14:23:42 +0200

    register elements, full fast twiss

--- xfields/prebuilt_kernel_definitions/element_inits.py
+++ xfields/prebuilt_kernel_definitions/element_inits.py
@@ -14,6 +14,12 @@ XFIELDS_ELEMENTS_INIT_DEFAULTS = {
         'other_beam_q0': 1.,
         'other_beam_beta0': 1.,
     },
+    'BeamBeamBiGaussianMultibunch2D': {
+        'num_bunches': 1,
+        'other_beam_q0': 1.,
+        'other_beam_beta0': 1.,
+        'other_beam_gamma0': 1.,
+    },
     'BeamBeamBiGaussian3D': {
         'slices_other_beam_zeta_center': np.array([0]),
         'slices_other_beam_num_particles': np.array([0]),
